2fbab98fd3ae0aa411334998025fe363af5f11f2
orasentidolhl99 <huongle17071999@gmail.com>
2020-03-02 15:38:37 +0700

    add file counter3.js

--- counter3.js
+++ counter3.js
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<script>
-		document.addEventListener('DOMContentLoaded',function(){
-			document.querySelector('button').onclick = count;
-		})
 
-		let counter = 0;
+document.addEventListener('DOMContentLoaded',function(){
+	document.querySelector('button').onclick = count;
+})
 
-		function count (){
-			counter++;
-			document.querySelector('#counter').innerHTML = counter;
+let counter = 0;
 
-			if (counter % 10 === 0) {
-				//alert(`Mày đếm tới số ${counter} là được rồi!`);
-				alert(`Counter is at ${counter}!`);
-			}
-		}
-	</script>
-	<title>My Web</title>
-</head>
-<body>
-		<h1 id="counter">1</h1>
-		<button onclick="count()">Click Here!</button>
-</body>
-</html>
+function count (){
+	counter++;
+	document.querySelector('#counter').innerHTML = counter;
+
+	if (counter % 10 === 0) {
+		//alert(`Mày đếm tới số ${counter} là được rồi!`);
+		alert(`Counter is at ${counter}!`);
+	}
+}
